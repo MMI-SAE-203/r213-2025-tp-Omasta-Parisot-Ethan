69e3d15487717329c502fd1bf287a5871e3da5c5
Omasta-Parisot-Ethan <ethan.omasta-parisot@edu.univ-fcomte.fr>
2025-01-31 09:16:33 +0100

    premier commit tp 3

---
+++ src/js/backend.mjs
@@ -0,0 +1,19 @@
+import PocketBase from 'pocketbase';
+
+const db = new PocketBase("http://127.0.0.1:8090/");
+
+export async function getOffres() {
+    try {
+        let data = await db.collection('Maisons').getFullList({
+            sort: '-created',
+        });
+        data = data.map((maison) => {
+            maison.image_url = db.files.getURL(maison, maison.images);
+            return maison;
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la liste des maisons', error);
+        return [];
+    }
+}
